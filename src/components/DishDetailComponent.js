dac175fbb51971f7a29156a81adca1d467e53dd6
Dhruv Chunawala <dhruv.chunawala@optum.com>
2020-04-11 12:46:51 -0400

    redux actions

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -27,6 +27,7 @@ export class CommentForm extends Component {
         this.toggleModal();
         console.log('Current State is: ' + JSON.stringify(values));
         alert('Current State is: ' + JSON.stringify(values));
+        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
     }
 
     render() {
@@ -103,7 +104,7 @@ function RenderDish({ dish }) {
     }
 }
 
-function RenderComments({ comments }) {
+function RenderComments({ comments, addComment, dishId }) {
     if (comments == null) {
         return (
             <div></div>
@@ -129,7 +130,7 @@ function RenderComments({ comments }) {
         <div className="col-12 col-md-5 m-1">
             <h4> Comments</h4>
             <ul className="list-unstyled">{comm}</ul>
-            <CommentForm />
+            <CommentForm dishId={dishId} addComment={addComment} />
         </div>
     );
 }
@@ -158,7 +159,9 @@ const DishDetail = (props) => {
             </div>
             <div className="row">
                 <RenderDish dish={props.dish} />
-                <RenderComments comments={props.comments} />
+                <RenderComments comments={props.comments}
+                    addComment={props.addComment}
+                    dishId={props.dish.id} />
             </div>
         </div>
     );
